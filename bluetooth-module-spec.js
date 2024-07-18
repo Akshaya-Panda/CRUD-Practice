201f0c66c1195957cc87cb6fe9aca7949e38d1b0
AKSHAYA PANDA <89294828+Akshaya-Panda@users.noreply.github.com>
2024-07-18 17:03:09 +0530

    Create bluetooth-module-spec.js

---
+++ bluetooth-module-spec.js
@@ -0,0 +1,17 @@
+describe('BluetoothModulesCtrl', function() {
+ 
+    beforeEach(angular.mock.module(require('./index').name))
+   
+    var scope, ctrl
+   
+    beforeEach(inject(function($rootScope, $controller) {
+      scope = $rootScope.$new()
+      ctrl = $controller('BluetoothModulesCtrl', {$scope: scope})
+    }))
+   
+    it('should ...', inject(function() {
+      expect(1).toEqual(1)
+   
+    }))
+   
+  })
