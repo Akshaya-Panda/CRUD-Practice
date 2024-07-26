a59a90b7146873e15b31a205712d0b65f5bf4442
AKSHAYA PANDA <89294828+Akshaya-Panda@users.noreply.github.com>
2024-07-26 15:19:12 +0530

    Update rpAudioModules.js

--- rpAudioModules.js
+++ rpAudioModules.js
@@ -1,4 +1,4 @@
-const util = require('util'); 
+const util = require('util');
 const _ = require('lodash');
 const Promise = require('bluebird');
 const uuid = require('uuid');
@@ -8,20 +8,16 @@ const wire = require('../../../wire');
 const wireutil = require('../../../wire/util');
 const apiutil = require('../../../util/apiutil');
 const request = require('request');
-
-
-
+ 
 const log = logger.createLogger('api:controllers:rpAudioModules');
- 
 module.exports = {
-  getModules:getModules,
-  getModuleById:getModuleById,
-  addModule:addModule,
-  updateModule:updateModule,
-  deleteModule:deleteModule,
-  validateIpPort:validateIpPort
+  getModules: getModules,
+  getModuleById: getModuleById,
+  addModule: addModule,
+  updateModule: updateModule,
+  deleteModule: deleteModule,
+  validateIpPort: validateIpPort
 };
- 
 function getModules(req, res) {
   dbapi.loadModules().then(modules => {
     res.json({
@@ -32,11 +28,9 @@ function getModules(req, res) {
     apiutil.internalError(res, 'Failed to load modules: ', err.stack);
   });
 }
- 
 function getModuleById(req, res) {
 const id = req.swagger.params.id.value;
- 
-  dbapi.loadModule (id).then(module => {
+  dbapi.loadModule(id).then(module => {
     if (module) {
       res.json({
         success: true,
@@ -52,7 +46,7 @@ const id = req.swagger.params.id.value;
     apiutil.internalError(res, 'Failed to get module: ', err.stack);
   });
 }
-
+ 
 function validateIpPort(localip, port, subroute) {
   const url = `http://${localip}:${port}`;
   const options = {
@@ -67,42 +61,32 @@ function validateIpPort(localip, port, subroute) {
         return reject(new Error(`Failed to reach ${url}: ${error.message}`));
       }
       if (response.statusCode === 200) {
-        dbapi.checkFields(localip, port,subroute).then(isUnique => {
+        dbapi.checkFields(localip, port, subroute).then(isUnique => {
           if (!isUnique) {
-            return reject(new Error('Local IP and port combination must be unique'));
+            return reject(new Error('Local IP, port, or subroute already exists'));
           }
           resolve(true);
-        }).catch(reject);x  
+        }).catch(reject);
       } else {
         reject(new Error('Invalid IP/Port combination'));
       }
     });
   });
 }
-
-function validateModule(module) {
-  
-  const subroutePattern = /^\/[^/]*\/$/;
  
+function validateModule(module) {
+  const subroutePattern = /^\/[^/]*\/$/;
   if (!subroutePattern.test(module.subroute)) {
     throw new Error('Subroute should start with a forward slash and end with a backward slash');
   }
-
-  return dbapi.checkFields(module.localip,module.port,module.subroute);
-    
-}
-
- function addModule(req, res) {
-  const module = req.body;
-  module.createdBy = req.user.email;
-  module.updatedBy = req.user.email;  
  
-  // dbapi.insertRpAudioModule(module).then(() => {
-  //   res.json({ success: true });
-  // }).catch(err => {
-  //   apiutil.internalError(res, 'Failed to add module: ', err.stack);
-  // });
-
+  return dbapi.checkFields(module.localip, module.port, module.subroute);
+}
+ 
+function addModule(req, res) {
+  const module = req.body;
+module.createdBy = req.user.email;
+module.updatedBy = req.user.email;
   validateModule(module).then(() => {
     return dbapi.insertRpAudioModule(module);
   }).then(() => {
@@ -115,24 +99,12 @@ function validateModule(module) {
     }
   });
 }
-
-
  
 function updateModule(req, res) {
   const module = req.body;
-  const id = req.swagger.params.id.value;
-  module.updatedBy = req.user.email;
+const id = req.swagger.params.id.value;
+module.updatedBy = req.user.email;
  
-  // dbapi.updateRpAudioModule(id, module).then(stats => {
-  //   if (stats.replaced) {
-  //     res.json({ success: true });
-  //   } else {
-  //     res.status(404).json({ success: false, message: 'Module not found' });
-  //   }
-  // }).catch(err => {
-  //   apiutil.internalError(res, 'Failed to update module: ', err.stack);
-  // });
-
   validateModule(module).then(() => {
     return dbapi.updateRpAudioModule(id, module);
   }).then(stats => {
@@ -148,12 +120,10 @@ function updateModule(req, res) {
       apiutil.internalError(res, 'Failed to update module: ', err.stack);
     }
   });
-
 }
  
 function deleteModule(req, res) {
-  const id = req.swagger.params.id.value;
- 
+const id = req.swagger.params.id.value;
   dbapi.removeModule(id).then(stats => {
     if (stats.deleted) {
       res.json({ success: true });
