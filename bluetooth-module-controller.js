390d66238496ea8f2e97c9528c5aac07b3e3243b
AKSHAYA PANDA <89294828+Akshaya-Panda@users.noreply.github.com>
2024-07-26 11:33:39 +0530

    Update bluetooth-module-controller.js

--- bluetooth-module-controller.js
+++ bluetooth-module-controller.js
@@ -1,20 +1,28 @@
 module.exports = function BluetoothModulesCtrl($scope, SettingsService) {
   $scope.bluetoothModules = [];
   $scope.newModule = {};
-  $scope.showGenerate = false; 
+  $scope.showGenerate = false;
   $scope.showGenerated = false;
+  $scope.editModule = false;
  
   function updateModules() {
+    
     const modules = SettingsService.get('bluetoothModules');
     $scope.bluetoothModules = modules ? modules : [];
   }
  
   $scope.removeModule = function(name) {
-    console.log("delete hoja bhai")
+    console.log("Deleting module:", name);
     const modules = SettingsService.get('bluetoothModules') || [];
     const updatedModules = modules.filter(module => module.name !== name);
     SettingsService.set('bluetoothModules', updatedModules);
-    updateModules(); 
+    updateModules();
+  };
+ 
+  $scope.editModuleDetails = function(module) {
+    $scope.newModule = angular.copy(module);
+    $scope.editModule = true;
+    $scope.showGenerate = true;
   };
  
   $scope.closeGenerated = function() {
@@ -23,19 +31,35 @@ module.exports = function BluetoothModulesCtrl($scope, SettingsService) {
     updateModules();
   };
  
-  $scope.addModule = function() {
+  $scope.addOrUpdateModule = function() {
+    
     if ($scope.bluetoothForm.$valid) {
       const modules = SettingsService.get('bluetoothModules') || [];
-      modules.push($scope.newModule);
+      if ($scope.editModule) {
+        
+        const index = modules.findIndex(m => m.name === $scope.newModule.name);
+        
+        if (index !== -1) {
+          
+          modules[index] = $scope.newModule;
+        }
+        
+        
+      } else {
+        modules.push($scope.newModule);
+      }
       SettingsService.set('bluetoothModules', modules);
       $scope.newModule = {};
       $scope.showGenerate = false;
+      $scope.editModule = false;
       updateModules();
     }
   };
  
   $scope.toggleGenerate = function() {
     $scope.showGenerate = !$scope.showGenerate;
+    $scope.editModule = false;
+    $scope.newModule = {};
   };
  
   $scope.$on('user.keys.bluetoothModules.generated', function(event, module) {
